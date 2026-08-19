9c27579a37f6735789ed72a9fadc232ca5d062d9
bdteragroup-max <bd.teragroup@gmail.com>
2026-08-19 11:29:27 +0700

    234th time

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['@sparticuz/chromium'],
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
